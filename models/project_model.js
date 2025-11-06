4ad258b82878bd866e0cc567b05bcbdaed48ed62
Somesh saini <someshsaini81@gmail.com>
2025-11-06 12:27:46 +0530

    update the model

--- models/project_model.js
+++ models/project_model.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
-const Project = new mongoose.Schema({
-    "project_name:": String,
-    "department": String,
-    "status": String,
-    "project_stages": [String],
-    "assignto": String,
-}, {timestamps: true})
+const ProjectSchema = new mongoose.Schema({
+  project_name: String,      // fixed: removed ":" typo
+  department: String,
+  status: String,
+  project_stages: [mongoose.Schema.Types.Mixed], // allows any type of JSON data
+  assignto: String,
+}, { timestamps: true });
 
-module.exports = mongoose.model("Project",Project);
-
+module.exports = mongoose.model("Project", ProjectSchema);
