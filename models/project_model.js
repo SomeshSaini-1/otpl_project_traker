40e468c9510a570b69b8e377422407f4ea6d9884
Somesh saini <someshsaini81@gmail.com>
2025-11-06 11:26:26 +0530

    update the model

--- models/project_model.js
+++ models/project_model.js
@@ -4,9 +4,9 @@ const Project = new mongoose.Schema({
     "project_name:": String,
     "department": String,
     "status": String,
-    "project_stages" : String,
-    "assignto":String,
-},{timestamps :true})
+    "project_stages": [String],
+    "assignto": String,
+}, {timestamps: true})
 
 module.exports = mongoose.model("Project",Project);
 
