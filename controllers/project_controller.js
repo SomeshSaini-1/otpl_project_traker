036ceab2f5e3cd722763e35eb335635ea3ba4dc7
Somesh saini <someshsaini81@gmail.com>
2025-11-06 13:34:26 +0530

    update the edit api

--- controllers/project_controller.js
+++ controllers/project_controller.js
@@ -46,10 +46,10 @@ exports.Delete_project = async (req, res) => {
 
 exports.update_Project = async (req, res) => {
     try {
-        const { project_name, department,project_stages, assignto, id } = req.body;
+        const { project_name, department,project_stages, status, assignto, id } = req.body;
         const pro = await Project.updateOne(
             { _id: id },
-            { $set: { project_name, department, project_stages, assignto } }
+            { $set: { project_name, department, project_stages, status, assignto } }
         );
         res.status(200).json({ message: "Project updated successfully", result: updateResult });
     } catch (error) {
